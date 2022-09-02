0352782f7e7c8e047729792c349a75cc24b47764
gabriel.kenzo <gabriel.kenzo@idetalctvm.com.br>
2022-09-02 09:40:45 -0300

    adding some queries in test

--- example_test.go
+++ example_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"go-ent-poc/ent"
+	"go-ent-poc/ent/todo"
 	"log"
 
 	"entgo.io/ent/dialect"
@@ -34,7 +35,54 @@ func Example_Todo() {
 		log.Fatalf("failed creating a todo: %v", err)
 	}
 	fmt.Printf("%d: %q\n", task2.ID, task2.Text)
-	// Output:
-	// 1: "Add GraphQL Example"
-	// 2: "Add Tracing Example"
+
+	// Connecting 2 Todos
+	if err := task2.Update().SetParent(task1).Exec(ctx); err != nil {
+		log.Fatalf("failed connecting todo2 to its parent: %v", err)
+	}
+
+	// Query all todo items.
+	items, err := client.Todo.Query().All(ctx)
+	if err != nil {
+		log.Fatalf("failed querying todos: %v", err)
+	}
+	for _, t := range items {
+		fmt.Printf("%d: %q\n", t.ID, t.Text)
+	}
+
+	// Query all todo items that depend on other items.
+	items, err = client.Todo.Query().Where(todo.HasParent()).All(ctx)
+	if err != nil {
+		log.Fatalf("failed querying todos: %v", err)
+	}
+	for _, t := range items {
+		fmt.Printf("%d: %q\n", t.ID, t.Text)
+	}
+
+	// Query all todo items that don't depend on other items and have items that depend them.
+	items, err = client.Todo.Query().
+		Where(
+			todo.Not(
+				todo.HasParent(),
+			),
+			todo.HasChildren(),
+		).
+		All(ctx)
+	if err != nil {
+		log.Fatalf("failed querying todos: %v", err)
+	}
+	for _, t := range items {
+		fmt.Printf("%d: %q\n", t.ID, t.Text)
+	}
+
+	// Get a parent item through its children and expect the
+	// query to return exactly one item.
+	parent, err := client.Todo.Query(). // Query all todos.
+						Where(todo.HasParent()). // Filter only those with parents.
+						QueryParent().           // Continue traversals to the parents.
+						Only(ctx)                // Expect exactly one item.
+	if err != nil {
+		log.Fatalf("failed querying todos: %v", err)
+	}
+	fmt.Printf("%d: %q\n", parent.ID, parent.Text)
 }
