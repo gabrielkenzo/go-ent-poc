a546475901fb77c52575d33eeed51bdecdad9247
gabriel.kenzo <gabriel.kenzo@idetalctvm.com.br>
2022-09-08 15:07:14 -0300

    adding some more queries

--- example_test.go
+++ example_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"testing"
 	"todo/ent"
+	"todo/ent/todo"
 
 	"entgo.io/ent/dialect"
 	_ "github.com/mattn/go-sqlite3"
@@ -28,9 +29,83 @@ func Example_Todo() {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
-	task1, err := client.Todo.Create().Save(ctx)
+	task1, err := client.Todo.Create().SetText("Add GraphQL Example").Save(ctx)
 	if err != nil {
 		log.Fatalf("failed creating a todo: %v", err)
 	}
-	fmt.Println(task1)
+	fmt.Printf("%d: %q\n", task1.ID, task1.Text)
+	task2, err := client.Todo.Create().SetText("Add Tracing Example").Save(ctx)
+	if err != nil {
+		log.Fatalf("failed creating a todo: %v", err)
+	}
+	fmt.Printf("%d: %q\n", task2.ID, task2.Text)
+
+	if err := task2.Update().SetParent(task1).Exec(ctx); err != nil {
+		log.Fatalf("failed connecting todo2 to its parent: %v", err)
+	}
+
+	fmt.Println("")
+	fmt.Println("Query 1")
+	Query1(ctx, client)
+	fmt.Println("")
+	fmt.Println("Query 2")
+	Query2(ctx, client)
+	fmt.Println("")
+	fmt.Println("Query 3")
+	Query3(ctx, client)
+	fmt.Println("")
+	fmt.Println("Query 4")
+	Query4(ctx, client)
+}
+
+// Query all todo items.
+func Query1(ctx context.Context, client *ent.Client) {
+	items, err := client.Todo.Query().All(ctx)
+	if err != nil {
+		log.Fatalf("failed querying todos: %v", err)
+	}
+	for _, t := range items {
+		fmt.Printf("%d: %q\n", t.ID, t.Text)
+	}
+}
+
+// Query all todo items that depend on other items.
+func Query2(ctx context.Context, client *ent.Client) {
+	items, err := client.Todo.Query().Where(todo.HasParent()).All(ctx)
+	if err != nil {
+		log.Fatalf("failed querying todos: %v", err)
+	}
+	for _, t := range items {
+		fmt.Printf("%d: %q\n", t.ID, t.Text)
+	}
+}
+
+// Query all todo items that don't depend on other items and have items that depend them.
+func Query3(ctx context.Context, client *ent.Client) {
+	items, err := client.Todo.Query().
+		Where(
+			todo.Not(
+				todo.HasParent(),
+			),
+			todo.HasChildren(),
+		).
+		All(ctx)
+	if err != nil {
+		log.Fatalf("failed querying todos: %v", err)
+	}
+	for _, t := range items {
+		fmt.Printf("%d: %q\n", t.ID, t.Text)
+	}
+}
+
+// Get a parent item through its children and expect the query to return exactly one item.
+func Query4(ctx context.Context, client *ent.Client) {
+	parent, err := client.Todo.Query(). // Query all todos.
+						Where(todo.HasParent()). // Filter only those with parents.
+						QueryParent().           // Continue traversals to the parents.
+						Only(ctx)                // Expect exactly one item.
+	if err != nil {
+		log.Fatalf("failed querying todos: %v", err)
+	}
+	fmt.Printf("%d: %q\n", parent.ID, parent.Text)
 }
